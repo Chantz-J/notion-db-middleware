d0bd2994595c4c6cd12a63fbb2a0779b7a65144b
Chantz-J <johnson.chantz@gmail.com>
2023-03-21 22:00:50 -0400

    added Add the Access-Control-Allow-Origin header to the server response manually..

--- app.js
+++ app.js
@@ -59,6 +59,12 @@ app.use('/notion', notionRouter)
 //   next();
 // });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
