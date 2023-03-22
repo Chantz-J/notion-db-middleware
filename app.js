4df526811e251fdb750609842bf101da02a67fce
Chantz-J <johnson.chantz@gmail.com>
2023-03-21 22:20:25 -0400

    Reduced Access Control

--- app.js
+++ app.js
@@ -49,10 +49,10 @@ app.use('/notion', notionRouter)
 //   next();
 // });
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   next();
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
